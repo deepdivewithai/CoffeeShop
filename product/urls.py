badf5440879ac1ebd9a84d22835a40c851c6db54
HEMANT <webdev.hemant@gmail.com>
2023-10-20 13:40:45 +0530

    product models

--- product/urls.py
+++ product/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('product_list/', views.ProductListView.as_view(), name='product_list'),
     path('product_list/<int:pk>', views.ProductDetailView.as_view(), name='product_details'),
     path('inventory/', views.InventoryListView.as_view(), name='inventory_list'),
-    path('inventory_list/<int:pk>', views.InventoryDetailView.as_view(), name='inventory_details'),
+    path('inventory/<int:pk>', views.InventoryDetailView.as_view(), name='inventory_details'),
 ]
